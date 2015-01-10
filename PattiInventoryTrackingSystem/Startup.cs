986a3af0f2d294652ffa8ec821652b6d69ca5066
jwparkersr <jparker@askcts.com>
2015-01-10 12:02:38 -0600

    Project Created

---
+++ PattiInventoryTrackingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PattiInventoryTrackingSystem.Startup))]
+namespace PattiInventoryTrackingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
